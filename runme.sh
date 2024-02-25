6755b4f7d5467c71bb90496bd291c962e16401e7
nireitdev <85206635+nireitdev@users.noreply.github.com>
2024-02-25 19:29:49 -0300

    fix bash

--- runme.sh
+++ runme.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #Simple ejemplo de parametros necesarios:
+
 PATH_FILEPASSW=rockyou.txt
 SERVER=192.168.111.104
 USER=admin
 PORT=2222
 THREADS=10
 
-go run main.go -h ${SERVER} -p ${PORT} -u ${USER} -t ${THREADS}
+go run main.go -f ${PATH_FILEPASSW} -h ${SERVER} -p ${PORT} -u ${USER} -t ${THREADS}
